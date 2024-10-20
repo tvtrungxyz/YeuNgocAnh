aa1cb0b8c1f8741d160f52d2978fe94aa9618ac6
tvtrungxyz <coconcact10@gmail.com>
2024-10-20 22:47:30 +0700

    “love”

---
+++ config.js
@@ -0,0 +1,12 @@
+const nameGirl = 'Ngoc Anh:3';
+const giftUrl = 'https://zalo.me/0582747090';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng babi Ngọc Ánh';
+const contentCard = 'Chúc mừng 20-10, Ngọc Ánh yêu dấu! 💐Nhân ngày đặc biệt này, anh chỉ muốn nói rằng anh rất biết ơn vì có em trong cuộc đời, Em là người con gái tuyệt vời nhất mà anh từng gặp, luôn mang đến cho anh niềm vui và hạnh phúc, Anh hứa sẽ luôn yêu thương, chăm sóc và bảo vệ em, Dù có ở xa, nhưng tình cảm anh dành cho em luôn gần gũi và ấm áp, Chúc em ngày 20-10 tràn đầy niềm vui, hạnh phúc và luôn tươi cười như những đóa hoa xinh đẹp, Yêu em rất nhiều! ❤️'
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// chiu
+// hihi
+const giftImage = 'ngocanh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
